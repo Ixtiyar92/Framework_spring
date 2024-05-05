b497d3fe72272c4e61910a9c5c92a484d191b848
AliyevIxtiyar <amfibiya-man@mail.ru>
2024-05-05 18:33:30 +0700

    Семинар 12

---
+++ Spring_homework12/src/test/java/com/example/Spring_homework12/SpringHw12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_homework12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHw12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
