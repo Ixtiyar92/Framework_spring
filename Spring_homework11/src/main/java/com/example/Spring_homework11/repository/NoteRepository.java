2a66e3dc7581cac0ed5f953b8ed05f5eac8f2411
AliyevIxtiyar <amfibiya-man@mail.ru>
2024-05-05 14:06:01 +0700

    Семинар 11

---
+++ Spring_homework11/src/main/java/com/example/Spring_homework11/repository/NoteRepository.java
@@ -0,0 +1,9 @@
+package com.example.Spring_homework11.repository;
+
+import com.example.Spring_homework11.models.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+}
